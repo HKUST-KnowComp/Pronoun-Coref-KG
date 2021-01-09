2e0a403b9c99d828c241fa0096323db9062e11bc
hongmingzhang <hzhangal0330@gmail.com>
2021-01-09 10:36:25 -0500

    update kg_elmo_representation

--- prepare_lm_embedding.py
+++ prepare_lm_embedding.py
@@ -81,11 +81,46 @@ def cache_large_dataset(data_path, session, token_ph, len_ph, lm_emb, out_file):
                 print("Cached {} documents in {}".format(doc_num + 1, data_path))
 file_names = ['conll_data/train.jsonlines',
               'conll_data/dev.jsonlines', 'conll_data/test.jsonlines', 'medical_data/train.jsonlines', 'medical_data/test.jsonlines']
+with open('final_kg.json', 'r') as f:
+    kg = json.load(f)
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 if __name__ == "__main__":
     token_ph, len_ph, lm_emb = build_elmo()
     with tf.Session() as session:
         session.run(tf.global_variables_initializer())
+        with h5py.File("elmo_kg_cache.hdf5", "w") as out_file:
+            all_mention_keys = list()
+            for r in kg:
+                for e in kg[r]:
+                    all_mention_keys.append('-'.join(e[0]))
+                    all_mention_keys.append('-'.join(e[1]))
+            all_mention_keys = list(set(all_mention_keys))
+            print('Start to prepare elmo for kg...')
+            for mention_key in tqdm(all_mention_keys):
+                if len(mention_key) == 0:
+                    # print('find one empty key')
+                    continue
+                tmp_sentence = [mention_key.split('-')]
+                max_sentence_length = max(len(s) for s in tmp_sentence)
+                tokens = [[""] * max_sentence_length for _ in tmp_sentence]
+                text_len = np.array([len(s) for s in tmp_sentence])
+                for i, sentence in enumerate(tmp_sentence):
+                    for j, word in enumerate(sentence):
+                        tokens[i][j] = word
+                tokens = np.array(tokens)
+                tf_lm_emb = session.run(lm_emb, feed_dict={
+                    token_ph: tokens,
+                    len_ph: text_len
+                })
+                # print(mention_key)
+                try:
+                    group = out_file.create_group(mention_key.replace('/', ':'))
+                    for i, (e, l) in enumerate(zip(tf_lm_emb, text_len)):
+                        e = e[:l, :, :]
+                        group[str(i)] = e
+                except ValueError:
+                    print('find one exception')
+                    continue
         with h5py.File("elmo_cache.hdf5", "w") as out_file:
             for json_filename in file_names:
                 print('we are working on file:', json_filename)
